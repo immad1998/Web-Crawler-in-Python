5a60f3723e170d8ee1928816c76d926b7d1141c3
immad1998 <55346504+immad1998@users.noreply.github.com>
2019-12-20 18:11:40 +0500

    Add files via upload

---
+++ server.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+
+# Import necessary libraries
+import socket
+import sys
+from _thread import *
+import threading
+from queue import Queue
+from spider import Spider
+from domain import *
+from general import *
+
+lock = threading.Lock() # For ensuring that only one client can be catered at a time
+PROJECT_NAME = 'hi'
+NUMBER_OF_THREADS = 8
+QUEUE_FILE = PROJECT_NAME + '/queue.txt'
+CRAWLED_FILE = PROJECT_NAME + '/crawled.txt'
+queue = Queue()
+
+
+# Create worker threads 
+def create_workers():
+	for _ in range(NUMBER_OF_THREADS):
+		t = threading.Thread(target=work)
+		t.daemon = True
+		t.start()
+
+
+# Do the next job in the queue
+def work():
+	while True:
+		url = queue.get()
+		Spider.crawl_page(threading.current_thread().name, url)
+		queue.task_done()
+
+
+# Each queued link is a new job
+def create_jobs():
+	for link in file_to_set(QUEUE_FILE):
+		queue.put(link)
+	queue.join()
+	crawl()
+
+
+# Check if there are items in the queue, if so crawl them
+def crawl():
+	queued_links = file_to_set(QUEUE_FILE)
+	if len(queued_links) > 0:
+		print(str(len(queued_links)) + ' links in the queue')
+		create_jobs()
+    
+# Necessary steps for creation of socket and maintenance of client-server communication
+
+host = '127.0.0.1'
+port = 65432
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try: # Bind to the socket
+    s.bind((host, port))
+except socket.error:
+    print("ERROR: Binding failed")
+    sys.exit()
+
+s.listen(5) # Maintain a queue of clientsthat the server can cater to at the moment
+
+
+def clientthread(conn, addr): # Function for communication between client and server
+	welcomemsg = '\nWelcome to the server, type bye to exit or type your url and hit enter!\n'
+	conn.sendall(welcomemsg.encode()) # Send the initial message
+
+	while True:
+		data = conn.recv(1024).decode()
+		exitCMD = 'bye'
+
+		if (data == exitCMD): # If the client sends a 'bye' then release the lock for the next client in queue and close this current connection
+			lock.release();
+			conn.close();
+			break;
+				
+		else:
+			HOMEPAGE = data
+			DOMAIN_NAME = get_domain_name(HOMEPAGE)
+			Spider(PROJECT_NAME, HOMEPAGE, DOMAIN_NAME)
+			create_workers()
+			crawl()
+			links = ''
+			for x in file_to_set(CRAWLED_FILE):
+				links += x 
+				links += '\n'
+			conn.sendall(links.encode()) #send all the links to client
+
+		if not data: # Release the lock if no data is received from client and close the connection by breaking the loop
+			lock.release()
+			break;
+
+	conn.close()
+
+
+while 1:
+	conn, addr = s.accept() # Accept the connection
+	lock.acquire() # Acquire the lock to cater one client at a time
+	print('\nConnected with ' + addr[0] + ':' + str(addr[1]))
+	start_new_thread(clientthread, (conn, addr)) # Call the function for communication
+
+s.close()
